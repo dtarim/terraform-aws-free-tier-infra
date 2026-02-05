32a8483a5c4aa98f3584923ccb84f31b659ae663
Didem TARIM <85922403+dtarim@users.noreply.github.com>
2026-02-05 15:56:01 +0100

    Add AWS EC2 instance resource for devops VM
    Add AWS EC2 instance resource configuration.

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+resource "aws_instance" "devops_vm" {
+  ami           = "ami-0a261c0e5f51090b1" # Amazon Linux 2 (eu-central-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "devops-free-tier-vm"
+  }
+}
